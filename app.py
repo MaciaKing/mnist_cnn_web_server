ddb21283a81f9fba64c2c296cbc09d47215ceafa
MaciaKing <maciasalvasalva@gmail.com>
2025-06-10 16:26:32 +0200

    [restructure]

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hola, Mundo!'
+    @app.route('/')
+    def home():
+        return 'Hola, M!'
+
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(host='0.0.0.0', port=8000, debug=True)
--- test/test_with_out_deploy.py
+++ test/test_with_out_deploy.py
@@ -1,30 +1,39 @@
 import tensorflow as tf
+from keras.layers import TFSMLayer
 import numpy as np
 from PIL import Image
 import os
 from tkinter.tix import IMAGE
 
-base_dir = os.path.dirname(os.path.abspath(__file__))
-
-model_to_deploy = tf.keras.models.load_model(base_dir+"/../modelo_completo_tf/1")
-
-
-# Ruta de las imágenes
-image_dir = base_dir+"/images/"
-
-# Iterar sobre las imágenes
-for image_name in os.listdir(image_dir):
-    image_path = os.path.join(image_dir, image_name)
-
+def predict(model_to_deploy, image_path):
     try:
-        # Cargar y preprocesar imagen
+        # Preproces
         img = Image.open(image_path).convert("L").resize((28, 28))
         img = np.array(img).astype("float32") / 255.0
         img = img.reshape(1, 28, 28, 1)
 
-        # Predecir
         pred = model_to_deploy.predict(tf.convert_to_tensor(img))
-        predicted_class = np.argmax(pred, axis=1)[0]
-        print(f'Imagen: {image_name} => Predicción: {predicted_class} => sin argmax: {pred}')
+        return np.argmax(pred, axis=1)[0]
+
     except Exception as e:
-        print(f"❌ Error con {image_name}: {e}")
+        print(f"❌ Error {e}")
+        return -1
+
+def get_class_image(image_path):
+    partes = image_path.split('_')
+    ultimo = partes[-1]
+    numero_str = ultimo.split('.')[0]
+    return int(numero_str)
+
+def test_image_predictions():
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    model_path = base_dir + "/../modelo_completo_tf/1"
+    model_to_deploy = tf.keras.Sequential([
+        TFSMLayer(model_path, call_endpoint="serving_default")
+    ])
+    image_dir = base_dir+"/images/"
+
+    for image_name in os.listdir(image_dir):
+        y_true = get_class_image(image_name)
+        predicted_class = predict(model_to_deploy, os.path.join(image_dir, image_name))
+        assert y_true == predicted_class
